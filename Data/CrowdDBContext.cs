50177573e02545d1076725f659231bbefcf7e21d
Thilina <hellowthilinafive@gmail.com>
2020-11-12 21:32:43 +0530

    add DBContext class and services

--- Data/CrowdDBContext.cs
+++ Data/CrowdDBContext.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using CrowdControl.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CrowdControl.Data
 {
-    public class CrowdDBContext
+    public class CrowdDBContext : DbContext
     {
+        public CrowdDBContext(DbContextOptions<CrowdDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; } // user table
     }
 }
