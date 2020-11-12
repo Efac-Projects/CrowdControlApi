c6df7f0d8c8233686aa4128228cb68334907b4e3
Kavindi <kavithyangisiribaddana2016@gmail.com>
2020-11-12 23:36:38 +0530

    Add appointment class

---
+++ Models/Appointments.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrowdControl.Models
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int BusinessId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public double Price { get; set; }
+	public double Total { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public boolean Approved { get; set; }
+        
+    }
+}
