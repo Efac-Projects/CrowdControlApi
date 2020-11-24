b5c016b63c32c9ec061a198dedb4f82aabbb482c
Thilina <hellowthilinafive@gmail.com>
2020-11-24 20:39:32 +0530

    last on is-2

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace CrowdControl
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
